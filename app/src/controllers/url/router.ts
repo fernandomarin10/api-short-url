a0f2396fa47a8f16baa0d3527ebd3ed742b0db2d
Fer <marinbcn169@gmail.com>
2024-01-21 09:58:32 +0100

    add catch protection to errors on process use cases

--- app/src/controllers/url/router.ts
+++ app/src/controllers/url/router.ts
@@ -10,14 +10,25 @@ const saveUrl: SaveUrl = new SaveUrl();
 
 
 router.get("/:urlHash", (req: Request, res: Response) => {
-    const hash = req.params.urlHash;
-    const urlFromHash = getUrlFromHash.run(hash);
-    res.status(200).json({"url": urlFromHash, hash: req.params.urlHash});
+    try{
+        const hash = req.params.urlHash;
+        const urlFromHash = getUrlFromHash.run(hash);
+        res.status(200).json({"url": urlFromHash, hash: req.params.urlHash});
+    }catch(e){
+        console.log("Error on GET petition / :>> ", e.toString());
+        res.status(500).json({error: e.toString()});;
+    }
+    
 });
 
 router.post("", (_req: Request, res: Response) => {
-    const hashFromUrl = saveUrl.run();
-    res.status(200).json({"hash": hashFromUrl});
+    try{
+        const hashFromUrl = saveUrl.run();
+        return res.status(200).json({"hash": hashFromUrl});
+    }catch(e){
+        console.log("Error on POST petition / :>> ", e.toString());
+        return res.status(500).json({error: e.toString()});
+    }
 });
 
 export default router;
